659360e80af07f2848cc6a6059e2452efea38e68
Alex Cohen <alex-dambros@hotmail.com>
2023-12-05 20:18:26 -0300

    Methods added in StudentController

--- API/Controllers/StudentController.cs
+++ API/Controllers/StudentController.cs
@@ -42,5 +42,69 @@ namespace API.Controllers
                 return BadRequest($"Error: {ex.Message}");
             }
         }
+
+        [HttpGet("ByDiscipline/{idDiscipline}")]
+        public async Task<IActionResult> GetByDisciplinaId(int idDiscipline)
+        {
+            try
+            {
+                return Ok(await repository.GetByDisciplinaId(idDiscipline));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostStudent(DTOStudent studentDTO)
+        {
+            try
+            {
+                return Ok(await repository.PostStudent(studentDTO));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutStudent(int idStudent, DTOStudent studentDTO)
+        {
+
+            try
+            {
+                var student = await repository.GetStudentById(idStudent);
+
+                if (student == null) return NotFound();
+
+                return Ok(await repository.PutStudent(studentDTO));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteStudent(int idStudent)
+        {
+
+            try
+            {
+                var student = await repository.GetStudentById(idStudent);
+
+                if (student == null) return NotFound();
+
+                return Ok(await repository.DeleteStudent(idStudent));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+
     }
 }
